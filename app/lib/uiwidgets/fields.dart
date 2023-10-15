ae1106bb9efebbdc5d1b809fa420aa71912b5489
Strobe <GarrettPrentice@tuta.io>
2023-10-15 13:55:21 -0700

    Commented up fields.dart, for easier readability, added a dispose function for resource cleanup

--- app/lib/uiwidgets/fields.dart
+++ app/lib/uiwidgets/fields.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+/// This is used as a password field, but can be used for any generic secrets
+/// It supports hintText from the TextFormField widget, and will display
+/// what you provide inside of the field
+///
+/// Use the validator to provide validation to your field, this is required
+/// and returns null if valid.
 class PasswordField extends StatefulWidget {
   final String hintText;
   final String? Function(String?)? validator;
@@ -14,14 +20,20 @@ class PasswordField extends StatefulWidget {
 }
 
 class _PasswordFieldState extends State<PasswordField> {
+  // This key is used only to differentiate it from everything else in the widget
+  // tree
   final _formKey = GlobalKey<FormState>();
+  // this is used to control and track the text that is in the field
   final passwordController = TextEditingController();
+  // This is used to requrest focus on the field
   final passwordFocusNode = FocusNode();
+  // We obscure text by default
   bool _isObscured = true;
 
   @override
   void dispose() {
     passwordController.dispose();
+    passwordFocusNode.dispose();
     super.dispose();
   }
 
@@ -30,9 +42,12 @@ class _PasswordFieldState extends State<PasswordField> {
     return Form(
       key: _formKey,
       child: TextFormField(
+        //  This will only attempt to validate the field if user interacted
             autovalidateMode: AutovalidateMode.onUserInteraction,
             obscureText: _isObscured,
             focusNode: passwordFocusNode,
+        // changes the keybaord that the system displays to one that supports
+        // email addressing with the @.
             keyboardType: TextInputType.emailAddress,
             controller: passwordController,
             decoration: InputDecoration(
@@ -46,11 +61,13 @@ class _PasswordFieldState extends State<PasswordField> {
               suffixIcon: IconButton(
                 padding: const EdgeInsetsDirectional.only(end: 12.0),
                 icon: _isObscured ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
+                // Updates the staet of the widget, requests a redraw.
                 onPressed: () {setState(() { _isObscured = ! _isObscured; }); },
               ),
               hintText: widget.hintText,
               icon: const Icon(Icons.lock),
             ),
+            // Call whatever function is supplied.
             validator: (value) => widget.validator!.call(value),
           )
     );
