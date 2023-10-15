a33d0f30893ee07b9649462aa54a0fa8ec71b71f
Benek6h <Parsomk33@gmail.com>
2023-10-15 02:07:54 -0700

    Implemented settings button that goes to new page and back button that goes back to the previous page

---
+++ app/lib/settingsButton.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+// Button for settings
+class SettingsButton extends StatelessWidget {
+  const SettingsButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Welcome Screen"),
+          backgroundColor: Colors.deepPurpleAccent,
+          shadowColor: Colors.orangeAccent,
+        ),
+        body: Container(
+            color: Colors.white54,
+            child: Center(
+              child: GestureDetector(   // Detect if pushed
+                  onTap: () {
+                    Navigator.push(     // Go to settings page
+                        context, MaterialPageRoute(builder: (context) => const Settings())
+                    );
+                  },
+                  child: Container(     // Decoration
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(50),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.deepPurpleAccent,
+                          spreadRadius: 3,
+                          blurRadius: 13,
+                          offset: Offset(5, 10),
+                        ),
+                      ],
+                    ),
+                    child: const Center(
+                      child: Text("Settings",
+                          style: TextStyle(
+                              color: Colors.deepPurple,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18)
+                      ),
+                    ),
+                  )
+              ),
+            )
+        )
+    );
+  }
+}
+
+// Settings Page and Back button
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(   // Contains the back button
+        title: const Text("Settings"),
+        backgroundColor: Colors.deepPurpleAccent,
+        shadowColor: Colors.orangeAccent,
+      ),
+    );
+  }
+}
