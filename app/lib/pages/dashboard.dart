db08bb3313fa34bc6fab31052ed5a0d135d4c3e8
Benek6h <Parsomk33@gmail.com>
2023-10-15 12:09:53 -0700

    fixed unused import

--- app/lib/pages/dashboard.dart
+++ app/lib/pages/dashboard.dart
@@ -25,6 +25,7 @@ class _DashboardPageState extends State<DashboardPage> {
                   ElevatedButton(onPressed: (){
                     Navigator.of(context).pushReplacement(EntriesPage.route());
                   }, child: const Text('nextPageEntries') ),
+                  const SettingsButton(),
                 ],
               ),
             )
