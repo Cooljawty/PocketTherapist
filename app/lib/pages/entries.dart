fb756d305ae7616a639cbdbecfe95566eed5a44e
Garrett Prentice <garrettprentice@tuta.io>
2023-10-16 22:09:29 -0700

    Update entries.dart
    This indentation makes it easier to read.

--- app/lib/pages/entries.dart
+++ app/lib/pages/entries.dart
@@ -22,10 +22,10 @@ class _EntriesPageState extends State<EntriesPage> {
           child: Column(
             children: [
               const Text('Entries'),
-							const DisplayCard(content: [
-								{"title": "Entry1", "body": "The First entry"}, 
-								{"title": "Entry2", "body": "And this is the second entry"},
-							]),
+		const DisplayCard(content: [
+			{"title": "Entry1", "body": "The First entry"}, 
+			{"title": "Entry2", "body": "And this is the second entry"},
+		]),
               ElevatedButton(onPressed: (){
                 Navigator.of(context).pushReplacement(PlansPage.route());
               }, child: const Text('nextPagePlans') )
