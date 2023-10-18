2e6560745425db4ee491bb413c43ae50fd71fc5d
Strobe <GarrettPrentice@tuta.io>
2023-10-18 07:21:24 -0700

    Temporary commit to transfer files to laptop

---
+++ app/lib/uiwidgets/decorations.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RandomQuoteGenerator extends StatefulWidget {
+  const RandomQuoteGenerator({super.key});
+
+  @override
+  State<RandomQuoteGenerator> createState() => _RandomQuoteGeneratorState();
+}
+
+class _RandomQuoteGeneratorState extends State<RandomQuoteGenerator> {
+  late String currentQuote;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
