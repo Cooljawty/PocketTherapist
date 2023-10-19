5623403f897aa943ef440fbb5e33969f27826b32
Jacari Harper <jacariguyman2@gmail.com>
2023-10-18 20:29:51 -0700

    Made EntryPage constructor const

---
+++ app/lib/pages/entry.dart
@@ -0,0 +1,36 @@
+import 'package:app/pages/entries.dart';
+import 'package:flutter/material.dart';
+
+class EntryPage<T> extends StatefulWidget {
+	final Map<String,String> entry;
+
+  static Route<dynamic> route({entry}) {
+    return MaterialPageRoute(builder: (context) => EntryPage(entry: entry));
+  }
+
+  const EntryPage({super.key, required this.entry});
+
+  @override
+  State<EntryPage> createState() => _EntryPageState();
+}
+
+class _EntryPageState extends State<EntryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+			body: SafeArea(
+				child: Column(
+						children: <Widget>[ 
+							Text( "${widget.entry['title']}"),
+							Text( "${widget.entry['entryText']}"), 
+
+							ElevatedButton(onPressed: (){
+								Navigator.of(context).pushReplacement(EntriesPage.route());
+
+							}, child: const Text('nextPageEntries') )
+						],
+				),
+			),
+    );
+  }
+}
