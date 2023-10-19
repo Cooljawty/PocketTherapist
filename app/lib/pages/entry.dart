5cc65301e7222eab774bd5725937e37d792e4e4f
Jacari Harper <jacariguyman2@gmail.com>
2023-10-18 20:41:51 -0700

    Added comments for EntryPage

--- app/lib/pages/entry.dart
+++ app/lib/pages/entry.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class EntryPage<T> extends StatefulWidget {
 	final Map<String,String> entry;
 
+	/// Route for navigator to open page with a given entry
   static Route<dynamic> route({entry}) {
     return MaterialPageRoute(builder: (context) => EntryPage(entry: entry));
   }
