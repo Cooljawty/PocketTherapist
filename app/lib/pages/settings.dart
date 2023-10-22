4d5bae96c34dcffe0a406697486b1165ff6c1b91
Caelan <caelanotoole@gmail.com>
2023-10-22 00:30:43 -0700

    Updated vault file button to be a container and not elevated button

--- app/lib/pages/settings.dart
+++ app/lib/pages/settings.dart
@@ -17,21 +17,32 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              const Text('Settings'),
-              ElevatedButton(onPressed: (){
-
-                Navigator.of(context).pushReplacement(CalendarPage.route());
-
-
-              }, child: const Text('nextPageCalendar') ),
-              const ElevatedButton(
-                onPressed: loadFile,
-                child: Text("Open Vault File"),
-              )
-            ],
-          ),
+          minimum: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text('Settings'),
+                ElevatedButton(onPressed: (){
+                  Navigator.of(context).pushReplacement(CalendarPage.route());
+                }, child: const Text('nextPageCalendar') ),
+                InkWell(
+                  onTap: loadFile,
+                  child: Container(
+                      width: 200,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.secondary,
+                          borderRadius: const BorderRadius.all(Radius.circular(15.0))),
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: Text("Open Vault File")
+                      )
+                  ),
+                )
+              ],
+            ),
+          )
         )
     );
   }
