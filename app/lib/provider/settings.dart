8ccaabfb32767c1e64a778cb1a1b5790187735f3
Strobe <GarrettPrentice@tuta.io>
2023-10-24 21:00:50 -0700

    Updated SettingsManager to reflect Sharedpreference changes.

--- app/lib/provider/settings.dart
+++ app/lib/provider/settings.dart
@@ -1,28 +1,38 @@
+import 'package:app/provider/theme_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsManager with ChangeNotifier {
-  static const String themeKey = 'theme';
-  static const String quotePathKey = 'quotePath';
-  static const String settingsPathKey = 'settingsPath';
-  static const String storagePathKey = 'storagePath';
+  static const String prefrencesPrefix = "pocket-therapist";
+  static const String lightOrDarkKey = 'theme';
+  static const String storageFile = 'storage';
   static const String fontScaleKey = 'fontScale';
   static const String encryptionToggleKey = 'encryption';
   static const String accentColorKey = "accent";
+  static const String initKey = "init";
 
   static SettingsManager? reference;
   late final SharedPreferences _preferences;
 
   SettingsManager () {
     if(reference != null) throw StateError("Only one SettingManager at one time!");
-    SharedPreferences.setPrefix("pocket-therapist");
-    _loadPrefernces();
+    SharedPreferences.setPrefix(prefrencesPrefix);
+    _loadPreferences();
     reference = this;
   }
 
-  _loadPrefernces() async {
+  _loadPreferences() async {
     _preferences = await SharedPreferences.getInstance();
-    if(_preferences.containsKey(""))
+    bool init = _preferences.containsKey(initKey);
+     if(!init) {
+      await _preferences.setBool(initKey, true);
+      await _preferences.setString(storageFile, "data.db");
+      await _preferences.setDouble(fontScaleKey, 1.0);
+      await _preferences.setBool(encryptionToggleKey, true);
+      await _preferences.setInt(accentColorKey, ThemeSettings.lightTheme.primaryColor.value);
+      await _preferences.setBool(lightOrDarkKey, true);
+    }
+
   }
   
   static Object getSetting(String key) {
