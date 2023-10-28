424b29d26601d2a2c3e35378e109fa39fd0593b3
ian5924 <ianwakayu@gmail.com>
2023-10-28 13:05:53 -0700

    Added swipe template.

--- app/lib/pages/entries.dart
+++ app/lib/pages/entries.dart
@@ -13,19 +13,125 @@ class EntriesPage extends StatefulWidget {
 }
 
 class _EntriesPageState extends State<EntriesPage> {
+  final items = List<String>.generate(5, (i) => 'Item $i');
+
+  //items: List<String>.generate(10000, (i) => 'Item $i'),
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        //appBar: AppBar(title: const Text('Dismissible Sample')),
         body: SafeArea(
-          child: Column(
-            children: [
-              const Text('Entries'),
-              ElevatedButton(onPressed: (){
+      child: Column(
+        children: [
+          const Text('Entries'),
+          ElevatedButton(
+              onPressed: () {
                 Navigator.of(context).pushReplacement(PlansPage.route());
-              }, child: const Text('nextPagePlans') )
-            ],
-          ),
-        )
+              },
+              child: const Text('nextPagePlans')),
+          Expanded(
+              child: ListView.builder(
+            itemCount: items.length,
+            /*prototypeItem: ListTile(
+                  title: Text(items.first),
+                ),*/
+            itemBuilder: (context, index) {
+              final item = items[index];
+              return Dismissible(
+                // Each Dismissible must contain a Key. Keys allow Flutter to
+                // uniquely identify widgets.
+                key: Key(item),
+                // Provide a function that tells the app
+                // what to do after an item has been swiped away.
+                onDismissed: (direction) {
+                  // Remove the item from the data source.
+                  setState(() {
+                    items.removeAt(index);
+                  });
+
+                  // Then show a snackbar.
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('$item dismissed')));
+                },
+                // Show a red background as the item is swiped away.
+                background: Container(color: Colors.red),
+                child: ListTile(
+                  title: Text(item),
+                ),
+              );
+            },
+          )
+              //ListViewBuilder(),
+              )
+        ],
+      ),
+    ));
+  }
+}
+
+/*
+
+
+
+// MyApp is a StatefulWidget. This allows updating the state of the
+// widget when an item is removed.
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  final items = List<String>.generate(20, (i) => 'Item ${i + 1}');
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Dismissing Items';
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Dismissible(
+              // Each Dismissible must contain a Key. Keys allow Flutter to
+              // uniquely identify widgets.
+              key: Key(item),
+              // Provide a function that tells the app
+              // what to do after an item has been swiped away.
+              onDismissed: (direction) {
+                // Remove the item from the data source.
+                setState(() {
+                  items.removeAt(index);
+                });
+
+                // Then show a snackbar.
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('$item dismissed')));
+              },
+              // Show a red background as the item is swiped away.
+              background: Container(color: Colors.red),
+              child: ListTile(
+                title: Text(item),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
+
+*/
