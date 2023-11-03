ea7a47e6cc63d6d3345837e923cf46cd5802cf03
Strobe <GarrettPrentice@tuta.io>
2023-11-03 12:22:19 -0700

    Partial encryption completed

--- app/lib/provider/encryptor.dart
+++ app/lib/provider/encryptor.dart
@@ -1,21 +1,84 @@
 import 'dart:collection';
 import 'dart:convert';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
+import 'package:cryptography/cryptography.dart';
+import 'package:encrypt/encrypt.dart';
+import 'package:system_info2/system_info2.dart';
 
-String _passwordHash = "";
-String _ivCipherText= "";
-String _keyCipherText= "";
+// Static Storage
+List<int> _passwordHash = [];
+List<int> _ivCipher= [];
+List<int> _keyCipher= [];
 
-void setPassword(String password) {
-  //Do hash things
+// Live - Only in memory
+SecretKey? keyEncryptionKey;
 
-  _passwordHash = password;
+// Only held during creation
+IV? signingIV;
+// Only held during creation
+Key? signingKey;
+// Held in memory
+Encrypter? encrypter;
+// Only saved during creation
+List<int>? nonce;
+
+Argon2id? hasher;
+
+
+/// If this is the first time opening the app, or encrypting is now enabled.
+/// Gen key => Encrypt KEy w/ password using PBKDF2 => Hash using Argon => Save Hash, save encrypted key.
+/// keep key in memory, toss password
+///
+/// Else, ask pass, or skip, no encryption,
+///   check hash, if correct, decrypt key, toss password, hold key mem.
+void setPassword(String password) async {
+  // Encryption Setup
+  {
+    Random rand = Random.secure();
+    nonce = List.generate(32, (index) => rand.nextInt(256));
+    // PBKDF2 setup
+    {
+      // Salt
+      // According to argon2 specs, #CPU cores x 2 (threads)
+      int parallelPower = SysInfo.cores.length * 2;
+      // Free memory in this application in bytes
+      int freeMemory = SysInfo.getFreeVirtualMemory();
+      // We will use no less than 500MB (500000 bytes) but no more than 1.5Gb (1_500_000 bytes)
+      // Most mobile platforms use 4-5Gb in just running android. 
+      // most mobile platform cap at 8GB of ram
+      int consumeMemory = min(max(freeMemory, 500000), 1500000);
+      int iterations = 8; // 2x recommended minimum
+      int hashLength = 512; // length of generated hash
+      hasher = Argon2id(
+        parallelism: parallelPower,
+        memory: consumeMemory,
+        iterations: iterations,
+        hashLength: hashLength,
+      );
+    }
+    
+    assert(hasher != null);
+
+    //Do hash things
+    keyEncryptionKey = await hasher!.deriveKeyFromPassword(
+      password: password,
+      nonce: nonce! 
+    );
+  }
+  signingKey = Key.fromSecureRandom(512);
+  encrypter = Encrypter(AES(signingKey!, mode: AESMode.gcm));
+  _passwordHash = await keyEncryptionKey!.extractBytes();
+
+  _keyCipher =  
+  _ivCipher = 
 }
 
+
 /// Password can be empty, this is only used if the password is supplied
-bool validatePasswordField(String password) => (password.length >= 10 &&                       // length must be 10+      AND
+bool validatePasswordField(String password) => (password.length >= 10 &&    // length must be 10+      AND
       password.contains(RegExp(r'[!@#$%^&*()]+')) &&  // contains special char   AND
       password.contains(RegExp(r'\d+')));           // contains at least 1 num
 
@@ -52,6 +115,9 @@ bool load(Map<String, dynamic> map) {
 }
 
 void reset(){
+  encrypter = null;
+  signingIV = null;
+  signingKey = null;
   _passwordHash = "";
   _ivCipherText = "";
   _keyCipherText = "";
@@ -62,6 +128,36 @@ UnmodifiableMapView<String, dynamic> save() =>
     "data": _compress(),
     "sig": sha512.convert(utf8.encode(_compress())).toString(),
   });
+
+bool unlock(String password){
+   
+
+
+}
+
+// Encryption -------------------------------------
+
+String encryptAES(String plainText) {
+  final encrypted = encrypter!.encrypt(plainText);
+  return encrypted.base64;
+}
+
+String decryptAES(String cipherTextb64) {
+  Encrypted cipherText = Encrypted.fromBase64(cipherTextb64);
+  final decrypted = encrypter!.decrypt(cipherText);
+  return decrypted;
+}
+
+
+// Hasing -----------------------------------------
+String hash(String input) {
+
+}
+
+
+
+
+
 
 // Encodings ---------------------------------------
 String _hexEncode(List<int> bytes) {
