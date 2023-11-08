66f173bb06b7f9d94856cb8f678ef971d804769b
Benek6h <Parsomk33@gmail.com>
2023-11-07 20:08:20 -0800

    Changed filter by day to week. A lot of math.

--- app/lib/pages/entries.dart
+++ app/lib/pages/entries.dart
@@ -5,40 +5,38 @@ import 'package:app/provider/settings.dart';
 import 'package:flutter/material.dart';
 
 class EntriesPage extends StatefulWidget {
-
   static Route<dynamic> route() {
     return MaterialPageRoute(builder: (context) => const EntriesPage());
   }
 
-	const EntriesPage({super.key});
+  const EntriesPage({super.key});
 
   @override
   State<EntriesPage> createState() => _EntriesPageState();
 }
 
 class _EntriesPageState extends State<EntriesPage> {
-  //Generated list of strings
+  //Generated list of journal entries
   static List<JournalEntry> entries = [
-    JournalEntry(title: "Entry 1", entryText: "", date: DateTime(2020, 2, 26)),                   // same day
-    JournalEntry(title: "Entry 2", entryText: "", date: DateTime(2020, 2, 26)),                   //--------------
-    JournalEntry(title: "Entry 3", entryText: "", date: DateTime(2021, 2, 26)),                 // same month
-    JournalEntry(title: "Entry 4", entryText: "", date: DateTime(2021, 2, 30)),                 //--------------
-    JournalEntry(title: "Entry 5", entryText: "", date: DateTime(2021, 3, 30)),
-    JournalEntry(title: "Entry 6", entryText: "", date: DateTime(2021, 5, 30)),
-    JournalEntry(title: "Entry 7", entryText: ":3", date: DateTime(2019, 2, 26)),
-    JournalEntry(title: "Entry 8", entryText: ">:3", date: DateTime(2023, 2, 26)),
+    JournalEntry(title: "Entry 1", entryText: '', date: DateTime(2020, 2, 15)),
+    JournalEntry(title: "Entry 2", entryText: '', date: DateTime(2020, 2, 18)),
+    JournalEntry(title: "Entry 3", entryText: '', date: DateTime(2020, 2, 19)),
+    JournalEntry(title: "Entry 4", entryText: '', date: DateTime(2020, 2, 21)),
+    JournalEntry(title: "Entry 5", entryText: '', date: DateTime(2020, 2, 25)),
+    JournalEntry(title: "Entry 6", entryText: '', date: DateTime(2020, 2, 27)),
+    JournalEntry(title: "Entry 7", entryText: '', date: DateTime(2020, 2, 29)),
   ];
   final items = entries;
 
   // Display options
-  List<String> displayOptions = ['Day', 'Month', 'Year'];
-  String? chosenDisplay = 'Day';
+  List<String> displayOptions = ['Week', 'Month', 'Year'];
+  String? chosenDisplay = 'Week';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
+        child: Column(
         children: [
           const Text('Entries'),
 
@@ -50,7 +48,8 @@ class _EntriesPageState extends State<EntriesPage> {
                   padding: const EdgeInsets.all(20.0),
                   child: ElevatedButton(
                       onPressed: () {
-                        Navigator.of(context).pushReplacement(PlansPage.route());
+                        Navigator.of(context)
+                            .pushReplacement(PlansPage.route());
                       },
                       child: const Text('nextPagePlans')),
                 ),
@@ -58,6 +57,7 @@ class _EntriesPageState extends State<EntriesPage> {
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
+                  // Dropdown for filter by date
                   child: DropdownButtonFormField<String>(
                     // Make the grey background
                     dropdownColor: Colors.grey.shade300,
@@ -67,14 +67,14 @@ class _EntriesPageState extends State<EntriesPage> {
                     value: chosenDisplay,
                     items: displayOptions
                         .map((item) => DropdownMenuItem<String>(
-                        value: item,
-                        child: Text(
-                          item,
-                          style: const TextStyle(color: Colors.black),
-                        )))
+                            value: item,
+                            child: Text(
+                              item,
+                              style: const TextStyle(color: Colors.black),
+                            )))
                         .toList(),
 
-                    // if changed set the new theme
+                    // if changed set new display option
                     onChanged: (item) => setState(() {
                       chosenDisplay = item;
                     }),
@@ -87,89 +87,85 @@ class _EntriesPageState extends State<EntriesPage> {
           //holds the list of entries
           Expanded(
               child: ListView.builder(
-							itemCount: items.length,
-							itemBuilder: (context, index) {
-                // Sort the Journal entries by most recent date
-                final sortedItems = items..sort((item1,item2) => item2.getDate().compareTo(item1.getDate()));
-                final item = sortedItems[index];
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+              // Sort the Journal entries by most recent date
+              final sortedItems = items
+                ..sort((item1, item2) => item2.getDate().compareTo(item1.getDate()));
 
-                // Dividers by day
-                bool isSameDate = true;
-                if(index ==0){
-                  isSameDate = false;
-                }else{
-                  isSameDate = item.getDate().isSameDate(sortedItems[index - 1].getDate(), chosenDisplay!);
-                }
-                if(index == 0 || !(isSameDate)){
-                  return Column(
-                    children: [
-                      Text((() {
-                        if(chosenDisplay == 'Day'){
-                            return '${item.getDate().formatDate()} ${item.getDate().day.toString()}, ${item.getDate().year.toString()}';
-                        }else if(chosenDisplay == 'Month'){
-                            return '${item.getDate().formatDate()} ${item.getDate().year.toString()}';
-                        }
-                        else{
-                            return item.getDate().year.toString();
-                        }
+              final item = sortedItems[index];
+              final time = item.getDate();
 
-                      })()),
-                      Dismissible(
+              // Dividers by filter
+              bool isSameDate = true;
+              if (index == 0) {       // if first in list
+                isSameDate = false;
+              } else {                // else check if same date by filters
+                isSameDate = time.isSameDate(sortedItems[index - 1].getDate(), chosenDisplay!);
+              }
+              if (index == 0 || !(isSameDate)) {    // if not same date or first in list make new list
+                return Column(
+                  children: [
+                    Text((() {
+                      // If weekly view, then calculate weeks of the year and display range in header
+                      if (chosenDisplay == 'Week') {
+                        DateTime firstOfYear = DateTime(DateTime.now().year, 1, 1);
+                        int weekNum = firstOfYear.getWeekNumber(firstOfYear, time);
+                        DateTime upper = firstOfYear.add(Duration(days: (weekNum * 7)));
+                        DateTime lower = upper.subtract(const Duration(days: 6));
 
-                        // Each Dismissible must contain a Key. Keys allow Flutter to
-                        // uniquely identify widgets.
-                        key: Key(item.getTitle()),
-                        //prevents right swipes
-                        direction: DismissDirection.endToStart,
+                        // Range for the week
+                        return '${lower.formatDate()} ${lower.day.toString()} - ${upper.formatDate()} ${upper.day.toString()}';
+                      } else if (chosenDisplay == 'Month') {
+                        // If monthly, only display month and year
+                        return '${time.formatDate()} ${time.year.toString()}';
+                      } else {
+                        // If yearly, only display year
+                        return time.year.toString();
+                      }
+                    })()),
+                    // Make Dismissible card per journal entry
+                    Dismissible(
+                      // Each Dismissible must contain a Key. Keys allow Flutter to
+                      // uniquely identify widgets.
+                      key: Key(item.getTitle()),  // this has to change later
+                      //prevents right swipes
+                      direction: DismissDirection.endToStart,
 
-                        // Provide a function that tells the app
-                        // what to do after an item has been swiped away.
-                        onDismissed: (direction) {
+                      // Provide a function that tells the app
+                      // what to do after an item has been swiped away.
+                      onDismissed: (direction) {
                         // Remove the item from the data source.
                         setState(() {
-                        items.removeAt(index);
+                          items.removeAt(index);
                         });
 
-                        // Then show a snackBar.
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(SnackBar(content: Text('${item.getTitle()} dismissed')));
-                        },
-                        // Show a red background as the item is swiped away.
-                        // background: Container(
-                        // 	color: Colors.red,
-                        // ),
-                        child: item.asDisplayCard(),
-                      ),
-                    ],
-                  );
-                } else{
-                  return Dismissible(
-
-                    // Each Dismissible must contain a Key. Keys allow Flutter to
-                    // uniquely identify widgets.
-                    key: Key(item.getTitle()),
-                    //prevents right swipes
-                    direction: DismissDirection.endToStart,
-
-                    // Provide a function that tells the app
-                    // what to do after an item has been swiped away.
-                    onDismissed: (direction) {
-                      // Remove the item from the data source.
-                      setState(() {
-                        items.removeAt(index);
-                      });
-
-                      // Then show a snackBar.
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(SnackBar(content: Text('${item.getTitle()} dismissed')));
-                    },
-                    // Show a red background as the item is swiped away.
-                    // background: Container(
-                    // 	color: Colors.red,
-                    // ),
-                    child: item.asDisplayCard(),
-                  );
-                }
+                        // Then show a snackBar w/ item name as dismissed message
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('${item.getTitle()} dismissed')));
+                      },
+                      // Show a red background as the item is swiped away.
+                      // background: Container(
+                      // 	color: Colors.red,
+                      // ),
+                      child: item.asDisplayCard(),
+                    ),
+                  ],
+                );
+              } else {  // if in the same filter header list, then just make a new entry
+                return Dismissible(
+                  key: Key(item.getTitle()),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    setState(() {
+                      items.removeAt(index);
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('${item.getTitle()} dismissed')));
+                  },
+                  child: item.asDisplayCard(),
+                );
+              }
             },
           )
               //ListViewBuilder(),
@@ -178,14 +174,18 @@ class _EntriesPageState extends State<EntriesPage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              // Plan, tag, save bar
               const Bar(),
+
+              // temp settings button
               FloatingActionButton(
                 //add key for testing
                 onPressed: () {
                   Navigator.push(
-                    // Go to settings page
+                      // Go to settings page
                       context,
-                      MaterialPageRoute(builder: (context) => const SettingsPage()));
+                      MaterialPageRoute(
+                          builder: (context) => const SettingsPage()));
                 },
                 tooltip: 'Settings',
                 shape: const CircleBorder(eccentricity: 1.0),
@@ -232,10 +232,11 @@ class Bar extends StatelessWidget {
   }
 }
 
-extension DateHelper on DateTime {
+extension Formatter on DateTime {
 
+  // Get the month string
   String formatDate() {
-    switch (month){
+    switch (month) {
       case 1: return 'January';
       case 2: return 'February';
       case 3: return 'March';
@@ -248,15 +249,32 @@ extension DateHelper on DateTime {
       case 10: return 'October';
       case 11: return 'November';
       case 12: return 'December';
-      default: return 'Date is Wrong';
+      default: return 'Date is Wrong';  // This should never happen
     }
   }
+
+  // Check if entries are in the same filter date
   bool isSameDate(DateTime other, String display) {
-    switch(display) {
-      case 'Day': return (year == other.year && month == other.month && day == other.day);
+    switch (display) {
+      // If week filter, then check if in the same year, month, and week
+      case 'Week':
+        final firstWeek = DateTime(DateTime.now().year, 1, 1);
+        return (year == other.year && month == other.month && (getWeekNumber(firstWeek, this) == getWeekNumber(firstWeek, other)));
+     // if month filter, then check for same year and month
       case 'Month': return (year == other.year && month == other.month);
+      // if year filter, then check for same year
       case 'Year': return (year == other.year);
-      default: return (year == other.year && month == other.month && day == other.day);
+      // This should never happen
+      default: return false;
     }
+  }
+
+  // get the week number for DateTime math in headers and filters
+  int getWeekNumber(DateTime start, DateTime end) {
+    start = DateTime(start.year, start.month, start.day);
+    end = DateTime(end.year, end.month, end.day);
+
+    // return the difference between the start and end date by week rounded up
+    return (end.difference(start).inDays / 7).ceil();
   }
 }
