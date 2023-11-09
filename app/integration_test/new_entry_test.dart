629293e75358318da387e98377856f5966fd40b7
Jacari Harper <jacariguyman2@gmail.com>
2023-11-08 20:23:42 -0800

    Updated new entry test to add tags
    The new_entry_test will also test adding tags to a journal entry.
    Both a custom tag and a prexisting tage should be added and displayed in
    the current entry page.

--- app/integration_test/new_entry_test.dart
+++ app/integration_test/new_entry_test.dart
@@ -1,54 +1,111 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
 import 'package:app/pages/new_entry.dart';
+import 'package:app/provider/settings.dart' as settings;
+import 'package:app/main.dart' as app;
 
 void main() {
+  tearDown(() async => await settings.reset());
+
   testWidgets('Test the elements of the new entry page', (WidgetTester tester) async {
-        //Create values for key
-        final journalInput = find.byKey(const ValueKey("journalInput"));
-        final titleInput = find.byKey(const ValueKey("titleInput"));
+    settings.setMockValues({
+      settings.configuredKey: true,
+      settings.encryptionToggleKey: false,
+    });
+    app.main();
 
-        final saveButton = find.byKey(const ValueKey("saveButton"));
-        final planButton = find.byKey(const ValueKey("planButton"));
-        final tagButton = find.byKey(const ValueKey("tagButton"));
+    //traverse to tag settings
+    await tester.pumpAndSettle();
+    Finder startbutton = find.byKey(const Key("Start_Button"));
+    await tester.tap(startbutton);
+    await tester.pumpAndSettle();
+		await tester.tap(find.byKey(Key("Navbar_Destination_Entries")));
+		await tester.pumpAndSettle();
+		await tester.tap(find.byKey(Key("New Entry")));
+		await tester.pumpAndSettle();
 
-        //Target the New Entry page
-        await tester.pumpWidget(const MaterialApp(home: NewEntryPage()));
+		//You are now in the new entry page
+		//Create values for key
+		final journalInput = find.byKey(const ValueKey("journalInput"));
+		final titleInput = find.byKey(const ValueKey("titleInput"));
+		final tagList = find.byKey(Key('tagList'));
 
-        //Find the 3 inputs
-        expect(titleInput, findsOneWidget);
-        expect(journalInput, findsOneWidget);
-        expect(tagButton, findsOneWidget);
+		final saveButton = find.byKey(const ValueKey("saveButton"));
+		final planButton = find.byKey(const ValueKey("planButton"));
+		final tagButton = find.byKey(const ValueKey("tagButton"));
 
-        expect(find.text('Save'), findsOneWidget);
-        expect(find.text('Tag'), findsOneWidget);
-        expect(find.text('Plan'), findsOneWidget);
+		//Find the 3 inputs
+		expect(titleInput, findsOneWidget);
+		expect(journalInput, findsOneWidget);
+		expect(tagButton, findsOneWidget);
+    expect(tagList, findsOneWidget);
 
-        // Test adding text to the title
-        await tester.tap(titleInput);
-        await tester.enterText(titleInput, "Title!");
-        await tester.pump();
-        var titleText = (titleInput.evaluate().single.widget as TextField)
-            .controller!.text;
-        expect(titleText, equals("Title!"));
+		expect(find.text('Save'), findsOneWidget);
+		expect(find.text('Tag'), findsOneWidget);
+		expect(find.text('Plan'), findsOneWidget);
 
-        // Test adding text to the journal entry
-        await tester.tap(journalInput);
-        await tester.enterText(journalInput, "Journal!");
-        await tester.pump();
-        var journalText = (journalInput.evaluate().single.widget as TextField)
-            .controller!.text;
-        expect(journalText, equals("Journal!"));
+		// Test adding text to the title
+		await tester.tap(titleInput);
+		await tester.enterText(titleInput, "Title!");
+		await tester.pump();
+		var titleText = (titleInput.evaluate().single.widget as TextField)
+			.controller!.text;
+		expect(titleText, equals("Title!"));
 
-        //Focus on the simulating tapping of the three buttons
+		// Test adding text to the journal entry
+		await tester.tap(journalInput);
+		await tester.enterText(journalInput, "Journal!");
+		await tester.pump();
+		var journalText = (journalInput.evaluate().single.widget as TextField)
+			.controller!.text;
+		expect(journalText, equals("Journal!"));
 
-        await tester.tap(planButton);
-        await tester.pump();
+		//Focus on the simulating tapping of the three buttons
 
-        await tester.tap(tagButton);
-        await tester.pump();
+		await tester.tap(planButton);
+		await tester.pump();
 
-        await tester.tap(saveButton);
-        await tester.pump();
-      });
+		//Testing tag creation
+		await tester.tap(tagButton);
+		await tester.pumpAndSettle();
+
+		//Enter a new tag name
+    final searchBar = find.byKey(const Key('Tag Search Bar'));
+    expect(searchBar, findsOneWidget);
+    await tester.enterText(searchBar, "testTag");
+    await tester.pumpAndSettle();
+
+		//Create tag with given name
+    final createTagButton = find.byKey(const Key('Create Tag'));
+    expect(createTagButton, findsOneWidget);
+    await tester.tap(createTagButton);
+    await tester.pumpAndSettle();
+
+		//Select pre-existing tag
+		final addedTag = settings.tagList.first;
+    final addedTagSelector = find.byKey(Key('Select ${addedTag.getName()} Button'));
+    expect(addedTagSelector, findsOneWidget);
+		await tester.tap(addedTagSelector);
+		await tester.pump();
+
+		//Apply tags to journal entry
+		final applyTagButton = find.byKey(Key('Apply Tags'));
+    expect(applyTagButton, findsOneWidget);
+		await tester.tap(applyTagButton);
+		await tester.pumpAndSettle();
+		
+		//Check that both tags are added
+		final listedTags = find.byKey(Key('tag'));
+    expect(applyTagButton, findsNWidgets(2));
+
+		await tester.tap(applyTagButton);
+		await tester.pumpAndSettle();
+
+
+		await tester.tap(saveButton);
+		await tester.pump();
+		});
 }
