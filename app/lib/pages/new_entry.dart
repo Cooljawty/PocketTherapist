4e273cd349888555a1351a5c945e6efb445f4eb8
Caelan <caelanotoole@gmail.com>
2023-11-10 13:18:33 -0800

    Emotion Wheel

--- app/lib/pages/new_entry.dart
+++ app/lib/pages/new_entry.dart
@@ -26,6 +26,7 @@ class _NewEntryPageState extends State<NewEntryPage> {
 
     return MultiSelectItem<Tag>(Tag(id: idx, name: val), val);
   }).toList();
+
   // Add text controllers to retrieve text data
   final titleController = TextEditingController();
   final journalController = TextEditingController();
@@ -42,182 +43,178 @@ class _NewEntryPageState extends State<NewEntryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
+          child: Column(children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height - 347,
+            child: Column(
               children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height - 347,
-                    child: Column(
-                      children: [
-                        const Text('New Entry'),
-                        // Title input text field
-                        Padding(
-                        padding: const EdgeInsets.all(20),
-                          child: TextField(
-                            controller: titleController,
-                            key: const Key("titleInput"),
-                            decoration: const InputDecoration(
-                              border: UnderlineInputBorder(),
-                              labelText: 'Title',
-                            ),
-                          ),
-                        ),
-
-            // Journal input text field
-                     Padding(
-                       padding: const EdgeInsets.all(20),
-                       child: TextField(
-                         controller: journalController,
-                         key: const Key("journalInput"),
-                         decoration: const InputDecoration(
-                           border: UnderlineInputBorder(),
-                           labelText: 'Journal Entry',
-                         ),
-                         maxLines: 8,
-                         minLines: 1,
-                       ),
-                     ),
-
-            // Temporary Tags
-                     Padding(
-                       padding: EdgeInsets.all(20),
-                       child: MultiSelectDialogField(
-                         items: _items,
-                         title: const Text("Tags"),
-                         listType: MultiSelectListType.LIST,
-                         decoration: BoxDecoration(
-                           color: Colors.blue.withOpacity(0.1),
-                           borderRadius: const BorderRadius.all(Radius.circular(40)),
-                           border: Border.all(
-                             color: Colors.blue,
-                             width: 2,
-                           ),
-                         ),
-                         buttonIcon: const Icon(
-                           Icons.tag,
-                           color: Colors.blue,
-                         ),
-                         buttonText: Text(
-                           "Tags",
-                           style: TextStyle(
-                             color: Colors.blue[800],
-                             fontSize: 16,
-                           ),
-                         ),
-                         onConfirm: (results) {
-                  //_selectedAnimals = results;
-                     },
-                       ),
-                     ),
-
-            // Emotions
-                     Padding(
-                       padding: EdgeInsets.all(20),
-                       child: MultiSelectDialogField(
-                         items: _items,
-                         title: const Text("Emotions"),
-                         listType: MultiSelectListType.LIST,
-                         decoration: BoxDecoration(
-                           color: Colors.blue.withOpacity(0.1),
-                           borderRadius: const BorderRadius.all(Radius.circular(40)),
-                           border: Border.all(
-                             color: Colors.blue,
-                             width: 2,
-                           ),
-                         ),
-                         buttonIcon: const Icon(Icons.tag, color: Colors.blue,),
-                         buttonText: Text(
-                           "Emotions",
-                           style: TextStyle(
-                             color: Colors.blue[800],
-                             fontSize: 16,
-                           ),
-                         ),
-                         onConfirm: (results) {
-                  //_selectedAnimals = results;
-                     },
-                       ),
-                     ),
-                      ],
-                    )
-                ),
-                CircularSeekBar(
-                  width: double.infinity,
-                  height: 250,
-                  progress: _progress,
-                  barWidth: 8,
-                  startAngle: 45,
-                  sweepAngle: 270,
-                  strokeCap: StrokeCap.butt,
-                  progressGradientColors: const [
-                    Colors.red,
-                    Colors.orange,
-                    Colors.yellow,
-                    Colors.green,
-                    Colors.blue,
-                    Colors.indigo,
-                    Colors.purple
-                  ],
-                  innerThumbRadius: 5,
-                  innerThumbStrokeWidth: 3,
-                  innerThumbColor: Colors.white,
-                  outerThumbRadius: 5,
-                  outerThumbStrokeWidth: 10,
-                  outerThumbColor: Colors.blueAccent,
-                  dashWidth: 1,
-                  dashGap: 2,
-                  animation: false,
-                  valueNotifier: _valueNotifier,
-                  child: Center(
-                    child: ValueListenableBuilder(
-                        valueListenable: _valueNotifier,
-                        builder: (_, double value, __) => Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text('${value.round()}'),
-                            Text('progress'),
-                          ],
-                        )
+                const Text('New Entry'),
+                // Title input text field
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    controller: titleController,
+                    key: const Key("titleInput"),
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Title',
                     ),
                   ),
                 ),
 
-                //Row for overflow widget
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                // Journal input text field
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    controller: journalController,
+                    key: const Key("journalInput"),
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Journal Entry',
+                    ),
+                    maxLines: 8,
+                    minLines: 1,
+                  ),
+                ),
+
+                // Temporary Tags
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: MultiSelectDialogField(
+                    items: _items,
+                    title: const Text("Tags"),
+                    listType: MultiSelectListType.LIST,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.1),
+                      borderRadius: const BorderRadius.all(Radius.circular(40)),
+                      border: Border.all(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                    ),
+                    buttonIcon: const Icon(
+                      Icons.tag,
+                      color: Colors.blue,
+                    ),
+                    buttonText: Text(
+                      "Tags",
+                      style: TextStyle(
+                        color: Colors.blue[800],
+                        fontSize: 16,
+                      ),
+                    ),
+                    onConfirm: (results) {
+                      //_selectedAnimals = results;
+                    },
+                  ),
+                ),
+
+                // Emotions
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: MultiSelectDialogField(
+                    items: _items,
+                    title: const Text("Emotions"),
+                    listType: MultiSelectListType.LIST,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.1),
+                      borderRadius: const BorderRadius.all(Radius.circular(40)),
+                      border: Border.all(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                    ),
+                    buttonIcon: const Icon(
+                      Icons.tag,
+                      color: Colors.blue,
+                    ),
+                    buttonText: Text(
+                      "Emotions",
+                      style: TextStyle(
+                        color: Colors.blue[800],
+                        fontSize: 16,
+                      ),
+                    ),
+                    onConfirm: (results) {
+                      //_selectedAnimals = results;
+                    },
+                  ),
+                ),
+              ],
+            )),
+        CircularSeekBar(
+          width: double.infinity,
+          height: 175,
+          progress: _progress,
+          maxProgress: 60,
+          barWidth: 8,
+          startAngle: 5,
+          sweepAngle: 360,
+          strokeCap: StrokeCap.butt,
+          progressGradientColors: const [
+            Colors.red,
+            Colors.orange,
+            Colors.yellow,
+            Colors.green,
+            Colors.blue,
+            Colors.indigo,
+            Colors.purple
+          ],
+          innerThumbRadius: 5,
+          innerThumbStrokeWidth: 3,
+          innerThumbColor: Colors.white,
+          outerThumbRadius: 5,
+          outerThumbStrokeWidth: 10,
+          outerThumbColor: Colors.blueAccent,
+          dashWidth: 20,
+          dashGap: 10,
+          animation: false,
+          valueNotifier: _valueNotifier,
+          child: Center(
+            child: ValueListenableBuilder(
+                valueListenable: _valueNotifier,
+                builder: (_, double value, __) => Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('${(value.round())}'),
+                      ],
+                    )),
+          ),
+        ),
+
+        //Row for overflow widget
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+                color: getCurrentTheme().colorScheme.background,
+                child: Row(
                   children: [
-                    Container(
-                        color: getCurrentTheme().colorScheme.background,
-                        child: Row(
-                          children: [
-                            OverflowBar(
-                              spacing: 50,
-                              overflowAlignment: OverflowBarAlignment.center,
-                              children: <Widget>[
-                                TextButton(
-                                    key: const Key("planButton"),
-                                    child: const Text('Plan'),
-                                    onPressed: () {}),
-                                TextButton(
-                                    key: const Key("tagButton"),
-                                    child: const Text('Tag'),
-                                    onPressed: () {}),
-                                TextButton(
-                                    key: const Key("saveButton"),
-                                    child: const Text('Save'),
-                                    onPressed: () {
-                                      Navigator.pop(context, getEntry());
-                                    }
-                                ),
-                              ],
-                            )
-                          ],
-                        )
+                    OverflowBar(
+                      spacing: 50,
+                      overflowAlignment: OverflowBarAlignment.center,
+                      children: <Widget>[
+                        TextButton(
+                            key: const Key("planButton"),
+                            child: const Text('Plan'),
+                            onPressed: () {}),
+                        TextButton(
+                            key: const Key("tagButton"),
+                            child: const Text('Tag'),
+                            onPressed: () {}),
+                        TextButton(
+                            key: const Key("saveButton"),
+                            child: const Text('Save'),
+                            onPressed: () {
+                              Navigator.pop(context, getEntry());
+                            }),
+                      ],
                     )
                   ],
-                ),
-              ]
-          )
-      ),
+                ))
+          ],
+        ),
+      ])),
     );
   }
 
