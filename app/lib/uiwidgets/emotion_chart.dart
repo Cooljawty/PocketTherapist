985d4a0ca9120c2a340c226ec40ce794c9781698
Jacari Harper <jacariguyman2@gmail.com>
2023-11-20 13:27:28 -0800

    Finilized design for emotion graphs

--- app/lib/uiwidgets/emotion_chart.dart
+++ app/lib/uiwidgets/emotion_chart.dart
@@ -2,6 +2,7 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
+//import 'package:fl_chart_app/presentation/resources/app_resources.dart';
 import 'package:fl_chart/fl_chart.dart';
 
 import 'package:app/helper/classes.dart';
@@ -34,6 +35,27 @@ class EmotionGraph extends StatefulWidget {
 class _EmotionGraphState extends State<EmotionGraph> {
 	static const MAX_STRENGTH = 60.0;
 
+	static const _months = [ null, 
+		"January", "February", 
+		"March", "April", "May", 
+		"June", "July", "August", 
+		"November", "October", "November", 
+		"December"
+	];
+	
+	String _daySuffix(day) => switch(day) {
+		1 => "1st",
+		2 => "2nd",
+		3 => "3rd",
+		_ => "${day}th"
+	};
+
+	/*
+	final _weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((day) {
+		return Expanded( child: Text(day, textAlign: TextAlign.center) );
+	}).toList();
+	*/
+
 	Map<String, List<FlSpot>> _emotionData = {};
 	/* 
 		"Emotion 1": [
@@ -56,6 +78,17 @@ class _EmotionGraphState extends State<EmotionGraph> {
 	double getX(JournalEntry entry) => entry.getDate().difference(widget.startDate).inDays.floorToDouble();
 	double getXFromDay(DateTime day) => day.difference(widget.startDate).inDays.toDouble().floorToDouble();
 
+	Widget _getTimeTitles(double value, TitleMeta meta) {
+		final day = widget.startDate.add(Duration(days: value.toInt())).day;	
+
+		return SideTitleWidget(
+			axisSide: meta.axisSide,
+			//Dont label last line if it's not the end of the week
+			child: (widget.startDate.add(Duration(days: value.floor())).weekday != widget.startDate.weekday) ? Text("") 
+				: Text("${widget.startDate.month}/${day}", style: Theme.of(context).textTheme.labelMedium),
+		);
+	}
+	
 	@override
 	Widget build(BuildContext context) {
 		final entries = entriesBetween(widget.startDate, widget.endDate); 
@@ -74,67 +107,105 @@ class _EmotionGraphState extends State<EmotionGraph> {
 			}
 		} 	
 
-		return AspectRatio( 
-			aspectRatio: 1.75,
-			child: Padding(
-				padding: const EdgeInsets.all(12.0),
-				child: switch(widget.type) {
-					//Time graph
-					GraphTypes.time => LineChart(
-						LineChartData(
-							titlesData: FlTitlesData(show: false),
-							minX: 0.0, maxX: getXFromDay(widget.endDate),
-							minY: 0.0, maxY: MAX_STRENGTH,
-							gridData: FlGridData(
-								show: true,
-								drawHorizontalLine: false,
-								drawVerticalLine: true,
-								verticalInterval: 1,
-								getDrawingVerticalLine: (_) => FlLine(
-									dashArray: [1, 0],
-									strokeWidth: 0.8,
-									color: Colors.grey, 
+		return Expanded(
+			child: Card( 
+				//aspectRatio: 1.75,
+				child: Padding(
+					padding: const EdgeInsets.all(12.0),
+					child: Column(
+						children: [
+							Container(
+								margin: const EdgeInsets.only(bottom: 10),
+								child: Text( 
+									"${_months[widget.startDate.month]} ${_daySuffix(widget.startDate.day)} - ${_daySuffix(widget.endDate.day)}", 
+									style: Theme.of(context).textTheme.titleLarge,
+									textAlign: TextAlign.center,
 								),
 							),
-							//Create a line for each emotion 
-							lineBarsData: _emotionData.entries.map((entry) => LineChartBarData(
-									show: entry.value.any((value) => value.y != 0),
-									spots: entry.value,
-									color: emotionlist[entry.key]!.color,
-									dotData: const FlDotData( show: false, ),
-									isCurved: true,
-									curveSmoothness: 0.5,
-									barWidth: 4.5,
-									preventCurveOverShooting: true,
-									preventCurveOvershootingThreshold: 2,
-								),
-							).toList(),
-						),
+							Expanded(
+								child: switch(widget.type) {
+									//Time graph
+									GraphTypes.time => LineChart(
+										LineChartData(
+											titlesData: FlTitlesData(
+												//Only show time at bottom
+												topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+												leftTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+												rightTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+												bottomTitles: AxisTitles(
+													sideTitles: SideTitles(
+														showTitles: true,
+														interval: 1,
+														getTitlesWidget: _getTimeTitles,
+													),
+												),
+											),
+											minX: 0.0, maxX: getXFromDay(widget.endDate),
+											minY: 0.0, maxY: MAX_STRENGTH,
+											gridData: FlGridData(
+												show: true,
+												drawHorizontalLine: false,
+												drawVerticalLine: true,
+												verticalInterval: 1,
+												getDrawingVerticalLine: (_) => FlLine(
+													dashArray: [1, 0],
+													strokeWidth: 0.8,
+													color: Colors.grey, 
+												),
+											),
+											//Create a line for each emotion 
+											lineBarsData: _emotionData.entries.map((entry) => LineChartBarData(
+													show: entry.value.any((value) => value.y != 0),
+													spots: entry.value,
+													color: emotionlist[entry.key]!.color,
+													dotData: const FlDotData( show: false, ),
+													isCurved: true,
+													curveSmoothness: 0.5,
+													barWidth: 4.5,
+													preventCurveOverShooting: true,
+													preventCurveOvershootingThreshold: 2,
+											)).toList(),
+										),
+									),
+									//Frequency graph
+									GraphTypes.frequency => Container(
+										padding: const EdgeInsets.only(top: 14,),
+										margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 24,),
+										decoration: ShapeDecoration(
+											color: Theme.of(context).colorScheme.background, 
+											shape: Theme.of(context).cardTheme.shape!,
+										),
+										child: RadarChart(
+											RadarChartData(
+												radarShape: RadarShape.polygon,
+												borderData: FlBorderData(show: true),
+												//titlePositionPercentageOffset: 0.068,
+												getTitle: (index, angel) => RadarChartTitle(text: _emotionData.keys.elementAt(index)),
+												//Hide value ticker
+												ticksTextStyle: const TextStyle(color: Colors.transparent, fontSize: 10),
+												tickBorderData: const BorderSide(color: Colors.transparent),
+												radarBorderData: BorderSide(color: Theme.of(context).canvasColor),
+												gridBorderData: BorderSide(color: Theme.of(context).canvasColor, width: 2),
+												dataSets: [
+													RadarDataSet(
+														entryRadius: 0.0,
+														//Summ up the strength of all entreis
+														dataEntries: _emotionData.entries.map((entry) {
+															//Radial equation
+															// sum (emotion.strength) / (max intensity * date_range)
+															final sum = entry.value.fold(0.0, (sum, strength) => sum += strength.y); 
+															return RadarEntry(value: sum / (MAX_STRENGTH * getXFromDay(widget.endDate)) );
+														}).toList(),
+													),
+												],
+											),
+										),
+									),
+								}
+							),
+						],
 					),
-					//Frequency graph
-					GraphTypes.frequency => RadarChart(
-						RadarChartData(
-							borderData: FlBorderData(show: false),
-							getTitle: (index, angel) => RadarChartTitle(text: _emotionData.keys.elementAt(index)),
-							//Hide value ticker
-							ticksTextStyle: const TextStyle(color: Colors.transparent, fontSize: 10),
-							tickBorderData: const BorderSide(color: Colors.transparent),
-							radarBorderData: const BorderSide(color: Colors.transparent),
-							gridBorderData: BorderSide(color: Theme.of(context).canvasColor, width: 2),
-							dataSets: [
-								RadarDataSet(
-									//Summ up the strength of all entreis
-									dataEntries: _emotionData.entries.map((entry) {
-										//Radial equation
-										// sum (emotion.strength) / (max intensity * date_range)
-										final sum = entry.value.fold(0.0, (sum, strength) => sum += strength.y); 
-										return RadarEntry(value: sum / (MAX_STRENGTH * getXFromDay(widget.endDate)) );
-									}).toList(),
-								),
-							],
-						),
-					),
-				}
+				),
 			),
 		);
 	}
@@ -216,33 +287,85 @@ List<JournalEntry> entriesBetween(DateTime start, DateTime end) {
 				]
 			),
 			JournalEntry(
-				title: "", entryText: "", 
-				date: DateTime(2023, 1, 4), 
+				title: "Day one entry 1", entryText: "", 
+				date: DateTime(2023, 1, 17), 
 				emotions: [
 					Emotion(
-						name: "Angry",
-						color: Colors.red,
+						name: "Sad",
+						color: Colors.blue,
+						strength: 60,
+					),
+				]
+			),
+			JournalEntry(
+				title: "Day one entry 2", entryText: "", 
+				date: DateTime(2023, 1, 25), 
+				emotions: [
+					Emotion(
+						name: "Sad",
+						color: Colors.blue,
 						strength: 30,
 					),
 				]
 			),
 			JournalEntry(
 				title: "", entryText: "", 
-				date: DateTime(2023, 1, 5), 
+				date: DateTime(2023, 1, 30), 
 				emotions: [
 					Emotion(
 						name: "Sad",
 						color: Colors.blue,
-						strength: 5,
+						strength: 30,
 					),
 					Emotion(
 						name: "Angry",
 						color: Colors.red,
-						strength: 18,
+						strength: 8,
 					),
 				]
 			),
-			/*
+			JournalEntry(
+				title: "Day thrree entry 1", entryText: "", 
+				date: DateTime(2023, 1, 26), 
+				emotions: [
+					Emotion(
+						name: "Sad",
+						color: Colors.blue,
+						strength: 15,
+					),
+					Emotion(
+						name: "Happy",
+						color: Colors.green,
+						strength: 30,
+					),
+				]
+			),
+			JournalEntry(
+				title: "Day thrree entry 2", entryText: "", 
+				date: DateTime(2023, 1, 12), 
+				emotions: [
+					Emotion(
+						name: "Angry",
+						color: Colors.red,
+						strength: 60,
+					),
+				]
+			),
+			JournalEntry(
+				title: "", entryText: "", 
+				date: DateTime(2023, 1, 11), 
+			),
+			JournalEntry(
+				title: "Happy day", entryText: "", 
+				date: DateTime(2023, 1, 24), 
+				emotions: [
+					Emotion(
+						name: "Happy",
+						color: Colors.green,
+						strength: 30,
+					),
+				]
+			),
 			JournalEntry(
 				title: "Out of range entry", entryText: "", 
 				date: DateTime(2023, 1, 14), 
@@ -264,9 +387,9 @@ List<JournalEntry> entriesBetween(DateTime start, DateTime end) {
 					),
 				]
 			),
-			*/
 		];
 
-	//testEntries.retainWhere((e) => ( e.getDate().compareTo(start) <= 0 && e.getDate().compareTo(start) >= 0));
+	testEntries.retainWhere((e) => ( !e.getDate().isBefore(start) && !e.getDate().isAfter(end)));
+	testEntries.sort((a, b) => a.getDate().compareTo(b.getDate()));
 	return testEntries;
 }
