3c4d5c64bd555ed32f39366bf11bb687df729f5e
Jacari Harper <jacariguyman2@gmail.com>
2023-11-26 12:58:59 -0800

    Changed emotion graph test to integration test
    It has also been updated to use the settings to determine which graph
    type shows up.

--- app/integration_test/graph_panel_test.dart
+++ app/integration_test/graph_panel_test.dart
@@ -1,102 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 import 'package:fl_chart/fl_chart.dart';
 
+import 'package:app/main.dart' as app;
 import 'package:app/pages/entry.dart';
 import 'package:app/uiwidgets/emotion_chart.dart';
+import 'package:app/provider/settings.dart' as settings;
 
 void main() {
   late Widget myApp;
 
-	/*
-	const testEntries = <JournalEntry>[
-    JournalEntry(
-			title: "Day one entry 1", entryText: "", 
-			date: DateTime(2023, 1, 1) 
-			tags: [],
-		),
-    JournalEntry(
-			title: "Day one entry 2", entryText: "", 
-			date: DateTime(2023, 1, 1) 
-			tags: [],
-		),
-    JournalEntry(
-			title: "", entryText: "", 
-			date: DateTime(2023, 1, 2) 
-			tags: [],
-		),
-    JournalEntry(
-			title: "Day thrree entry 1", entryText: "", 
-			date: DateTime(2023, 1, 3) 
-			tags: [],
-		),
-    JournalEntry(
-			title: "Day thrree entry 2", entryText: "", 
-			date: DateTime(2023, 1, 3) 
-			tags: [],
-		),
-    JournalEntry(
-			title: "", entryText: "", 
-			date: DateTime(2023, 1, 4) 
-			tags: [],
-		),
-    JournalEntry(
-			title: "", entryText: "", 
-			date: DateTime(2023, 1, 5) 
-			tags: [],
-		),
-    JournalEntry(
-			title: "Out of range entry", entryText: "", 
-			date: DateTime(2023, 1, 14) 
-			tags: [],
-		),
-	];
-	*/
-
   setUp(() {
-    myApp = MaterialApp(
-      home: Scaffold(
-				body: SafeArea(
-					child: Column(
-						children: [
-							EmotionGraph(
-								key: const Key('Time graph'), 
-								startDate: DateTime(2023, 12, 1),
-								endDate:DateTime(2023, 12, 7),
-								type: GraphTypes.time
-							),
-							EmotionGraph(
-								key: const Key('Frequency graph'), 
-								startDate: DateTime(2023, 12, 1),
-								endDate:DateTime(2023, 12, 7),
-								type: GraphTypes.frequency
-							),
-						],
-					)
-				)
-			)
-		);
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   });
 
+  tearDown(() async {
+    await settings.reset();
+  });
+
+
   testWidgets('Displaying emotion time chart', (tester) async {
-    await tester.pumpWidget(myApp);
+    app.main();
+		await tester.pump();
+
+		settings.setMockValues({
+			'emotions': {
+				'Anger': Colors.red,
+				'Sad': Colors.blue,
+				'Happy': Colors.green,
+			},
+			'emotionGraphType': 'time',
+			'configured': true,
+			'encryption': false,
+		});
+		await tester.pump();
+
+		//Navigate to graph page
+		await tester.tap(find.byKey(Key("Start_Button")));
+    await tester.pumpAndSettle();
+		await tester.tap(find.byKey(Key("Navbar_Destination_Calendar")));
     await tester.pumpAndSettle();
 
 		//Two graphs should exist
-		expect(find.byType(EmotionGraph), findsNWidgets(2));
+		expect(find.byType(EmotionGraph), findsOneWidget);
 
 		//Check time chart
-		final timeGraph = find.byType(LineChart);
-		expect(timeGraph, findsOneWidget);
-		//Check labels
-		//Check axies
+		final emotionGraph = find.byType(LineChart);
+		expect(emotionGraph, findsOneWidget);
+	});
 
-		//Check frequency chart
-		final frequencyGraph = find.byType(RadarChart);
-		expect(frequencyGraph, findsOneWidget);
-		//Check labels
-		//Check axies
+  testWidgets('Displaying emotion frequency chart', (tester) async {
+    app.main();
+		await tester.pump();
+
+		settings.setMockValues({
+			'emotions': {
+				'Anger': Colors.red,
+				'Sad': Colors.blue,
+				'Happy': Colors.green,
+			},
+			'emotionGraphType': 'frequency',
+			'configured': true,
+			'encryption': false,
+		});
+		await tester.pump();
+
+		//Navigate to graph page
+		await tester.tap(find.byKey(Key("Start_Button")));
+    await tester.pumpAndSettle();
+		await tester.tap(find.byKey(Key("Navbar_Destination_Calendar")));
+    await tester.pumpAndSettle();
+
+		//Two graphs should exist
+		expect(find.byType(EmotionGraph), findsOneWidget);
+
+		//Check time chart
+		final emotionGraph = find.byType(RadarChart);
+		expect(emotionGraph, findsOneWidget);
 	});
 
 }
