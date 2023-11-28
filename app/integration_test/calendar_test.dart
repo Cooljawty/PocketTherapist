6f6d4f1a5f5a4da943367cf4dc3d9c9354e22174
Jacari Harper <jacariguyman2@gmail.com>
2023-11-27 18:18:34 -0800

    Updated calendar tests

--- app/integration_test/calendar_test.dart
+++ app/integration_test/calendar_test.dart
@@ -1,13 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'test_utils.dart';
 
 import 'package:flutter/material.dart';
+
 import 'package:app/pages/calendar.dart';
-import 'package:app/provider/entry.dart';
+import 'package:app/pages/entries.dart';
 import 'package:app/uiwidgets/decorations.dart';
 
 import 'package:app/provider/settings.dart' as settings;
 import 'package:app/main.dart' as app;
+import 'package:app/provider/entry.dart';
 
 void main() {
 
@@ -63,8 +66,7 @@ void main() {
 			final totalDays = firstWeekPadding + (lastOfTheMonth.difference(firstOfTheMonth).inDays + 1) + lastWeekPadding;
 
 			//Go to next month and test amount of days are correct
-			await tester.tap(nextButton);
-			await tester.pump();
+			await tap(tester, nextButton);
 			final calendarDays = find.byKey(const Key("Calendar_Day"));
 			expect(calendarDays, findsNWidgets(totalDays));
 		}
@@ -91,120 +93,58 @@ void main() {
 			final totalDays = firstWeekPadding + (lastOfTheMonth.difference(firstOfTheMonth).inDays + 1) + lastWeekPadding;
 
 			//Go to previous month and test amount of days are correct
-			await tester.tap(previousButton);
-			await tester.pump();
+			await tap(tester, previousButton);
 			final calendarDays = find.byKey(const Key("Calendar_Day"));
 			expect(calendarDays, findsNWidgets(totalDays));
 		}
 	});
 
-	Future<void> createNewEntry(WidgetTester tester) async {
-		// Find the nav bar button for entries page
-		await tester.tap(find.byKey(const Key("Navbar_Destination_Entries")));
-		await tester.pumpAndSettle();
-		await tester.tap(find.byKey(const Key("New Entry")));
-		await tester.pumpAndSettle();
-
-		const newTitle = "Title!";
-		const newEntry = "Journal!";
-
-		// Test adding title to the title field
-		await tester.tap(find.byKey(const Key("titleInput")));
-		await tester.enterText(find.byKey(const Key("titleInput")), newTitle);
-		await tester.pump();
-
-		// Test adding text to the journal entry field
-		await tester.tap(find.byKey(const Key("journalInput")));
-		await tester.enterText(find.byKey(const Key("journalInput")), newEntry);
-
-		// Tap the emotion button to bring up the emotion menu
-		await tester.tap(find.byKey(const Key("emotionButton")));
-		await tester.pumpAndSettle();
-
-		final emotionKey = find.text(emotionList.entries.first.key);
-		await tester.tap(emotionKey);
-		await tester.pumpAndSettle();
-
-		// Tap on confirm button
-		final confirmEmotionsButton = find.byKey(const Key('saveEmotionsButton'));
-		await tester.tap(confirmEmotionsButton);
-		await tester.pumpAndSettle();
-
-		// Find the chip display
-		final emotionChips = find.byKey(const Key('EmotionChipsDisplay'));
-
-		final emotionName = find.text(emotionList.entries.first.key);
-		await tester.pumpAndSettle();
-
-
-		// Tap the emotion chip to pull up emotional dial
-		await tester.tap(emotionName);
-		await tester.pumpAndSettle();
-
-		// find the emotionalDial
-		final emotionalDial = find.byKey(const Key('EmotionalDial'));
-
-		// Set a value on the emotional dial
-		await tester.drag(find.byKey(const Key('EmotionalDial')), const Offset(100, 0));
-		await tester.pumpAndSettle();
-
-		// Save Emotion strength
-		final saveDial = find.byKey(const Key('saveDial'));
-		await tester.tap(saveDial);
-		await tester.pumpAndSettle();
-
-		//Save new entry
-		await tester.tap(find.byKey(const Key("saveButton")));
-		await tester.pumpAndSettle();
-	}
-
   testWidgets('Calendar integration test', (WidgetTester tester) async {
-    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-		app.main();
-		await tester.pump();
-
-		// Set mock values in the settings
-		settings.setMockValues({
+		
+		await startAppWithSettings(tester, {
 			settings.configuredKey: true,
 			settings.encryptionToggleKey: false,
 		});
+
+		final today = DateTime.now();
 		emotionList = {
 			'Happy': const Color(0xfffddd68),
 		};
-
-		// Enter the app
-		Finder startButton = find.byKey(const Key("Start_Button"));
-		await tester.tap(startButton);
-
-		do{
-			await tester.pump();
-		}while(tester.widgetList(find.byKey(const Key("Navbar_Destination_Entries"))).isEmpty);
-
-		await createNewEntry(tester);
-
-		//Navigate to Calendar	
-		await tester.tap(find.byKey(const Key("Navbar_Destination_Calendar")));
-		await tester.pumpAndSettle();
-
+		entries = [
+			JournalEntry(
+				title: "Title!",
+				entryText: "Journal!",
+				date: today,
+				emotions: [ 
+					Emotion(
+						name: "Happy", 
+						color: const Color(0xfffddd68), 
+						strength: 50
+					),
+				]
+			),
+		];
+		await skipToCalendarPage(tester);
 
 		//Open todays entries(s)
 		final calendarDays = find.byKey(const Key("Calendar_Day"));
-		final firstWeekPadding = DateTime(DateTime.now().year, DateTime.now().month, 1).weekday - 1;
-		final today = tester.widgetList(calendarDays).elementAt(firstWeekPadding + DateTime.now().day - 1);
+		await pumpUntilFound(tester, calendarDays);
+
+		final firstOfTheMonth = DateTime(today.year, today.month, 1);
+		final firstWeekPadding = firstOfTheMonth.weekday - 1;
+		final todayItem = tester.widgetList(calendarDays).elementAt(firstWeekPadding + today.day - 1) as Container;
 
 		//Today should be colored according to the entry we just made
-		final todayColor = ((today as Container).decoration as ShapeDecoration).color; 
+		final todayColor = (todayItem.decoration as ShapeDecoration).color; 
 		expect(todayColor, emotionList["Happy"]);
 
-		await tester.tap(find.byWidget(today));
-		await tester.pumpAndSettle();
+		await tap(tester, find.byWidget(todayItem));
+		await pumpUntilFound(tester, find.byType(EntryPanelPage)); 
 
 		// View new entry
-		final card = find.byType(DisplayCard).first;
+		final card = find.byType(DisplayCard);
 		expect(card, findsWidgets);
-		await tester.tap(card);
-		await tester.pumpAndSettle();
+		await tap(tester, card);
 		expect(find.text("Title!"), findsOneWidget);
 		expect(find.text("Journal!"), findsOneWidget);
   });
