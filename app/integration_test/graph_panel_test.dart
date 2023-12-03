7adbfa9eec0ea0dba60fbe5ea9db7c7abf23f29c
JikoJeans <miguelmung5@gmail.com>
2023-12-03 14:44:14 -0800

    update for test coverage fix
    added in emotions for loading during the test.

--- app/integration_test/graph_panel_test.dart
+++ app/integration_test/graph_panel_test.dart
@@ -12,40 +12,56 @@ import 'test_utils.dart';
 
 void main() {
   testWidgets('Displaying emotion time chart', (tester) async {
-		//Navigate to graph page
-		await startSkipFrontScreen(tester);
-		await pumpUntilFound(tester, find.byKey(const Key("navCalendar")), true);
-		await tap(tester, find.byKey(const Key("navCalendar")), true);
+    //Navigate to graph page
+    await startSkipFrontScreen(tester);
+    await pumpUntilFound(tester, find.byKey(const Key("navCalendar")), true);
+    await tap(tester, find.byKey(const Key("navCalendar")), true);
 
-		//Two graphs should exist
-		expect(find.byType(EmotionGraph), findsOneWidget);
+    //Two graphs should exist
+    expect(find.byType(EmotionGraph), findsOneWidget);
 
-		//Check time chart
-		final emotionGraph = find.byType(LineChart);
-		expect(emotionGraph, findsOneWidget);
-	});
+    //Check time chart
+    final emotionGraph = find.byType(LineChart);
+    expect(emotionGraph, findsOneWidget);
+  });
 
   testWidgets('Displaying emotion frequency chart', (tester) async {
-		await startAppWithSettings(tester, {
-			settings.configuredKey: true,
-			settings.encryptionToggleKey: false,
-			settings.emotionGraphTypeKey: GraphTypes.frequency.toString()
-		});
+    //await startAppWithSettings(tester, {
+    //  settings.configuredKey: true,
+    //  settings.encryptionToggleKey: false,
+    //  settings.emotionGraphTypeKey: GraphTypes.frequency.toString()
+    //});
+    Map<String, Object> settingsMap = {
+      "configured": true,
+      "encryption": false,
+      "emotionGraphType": GraphTypes.frequency.toString(),
+      //load emotions for map
+      "emotions": [
+        "Happy",
+        "Trust",
+        "Fear",
+        "Sad",
+        "Disgust",
+        "Anger",
+        "Anticipation",
+        "Surprise"
+      ]
+    };
+    await startAppWithSettings(tester, settingsMap);
 
-		// Enter the app
-		Finder startButton = find.byKey(const Key("Start_Button"));
-		await pumpUntilFound(tester, startButton);
-		await tap(tester, startButton);
+    // Enter the app
+    Finder startButton = find.byKey(const Key("Start_Button"));
+    await pumpUntilFound(tester, startButton);
+    await tap(tester, startButton);
 
+    await pumpUntilFound(tester, find.byKey(const Key("navCalendar")), true);
+    await tap(tester, find.byKey(const Key("navCalendar")), true);
+    await tester.pumpAndSettle();
+    //Two graphs should exist
+    expect(find.byType(EmotionGraph), findsOneWidget);
 
-		await pumpUntilFound(tester, find.byKey(const Key("navCalendar")), true);
-		await tap(tester, find.byKey(const Key("navCalendar")), true);
-		await tester.pumpAndSettle();
-		//Two graphs should exist
-		expect(find.byType(EmotionGraph), findsOneWidget);
-
-		//Check time chart
-		final emotionGraph = find.byType(RadarChart);
-		expect(emotionGraph, findsOneWidget);
-	});
+    //Check time chart
+    final emotionGraph = find.byType(RadarChart);
+    expect(emotionGraph, findsOneWidget);
+  });
 }
