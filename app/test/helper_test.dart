a4e49c80aeaa79f21084c6095b20ef734f96bf4a
Strobe <GarrettPrentice@tuta.io>
2023-12-02 21:33:13 -0800

    updated codde coverage, it doesnt seem to be accruate

--- app/test/helper_test.dart
+++ app/test/helper_test.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 
 import 'package:app/helper/dates_and_times.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,7 +8,6 @@ void main() {
       DateTime beforeTwo = DateTime.now();
       DateTime beforeThree = DateTime.now().add(const Duration(seconds: 5));
       DateTime beforeFour = DateTime(2000, 1, 31);
-      DateTime badBeforeFour = DateTime(2000, 1, 32);
       expect(before.isSameDate(beforeTwo, 'Week'), true);
       expect(before.isSameDate(beforeTwo, 'Month'), true);
       expect(before.isSameDate(beforeTwo, 'Year'), true);
@@ -17,7 +15,6 @@ void main() {
       expect(before.isSameDate(beforeFour, 'Year'), false);
       expect(before.isSameDate(beforeFour, 'Month'), false);
       expect(before.isSameDate(beforeFour, 'Day'), false);
-      expect(beforeFour.formatDate().day == "31st", true);
   });
 
 }
