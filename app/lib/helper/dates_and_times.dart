da721431f08fe40107f6b96f56f8dd2eaa71fe10
Jacari Harper <jacariguyman2@gmail.com>
2023-12-04 16:57:47 -0800

    Ficed days in month method

--- app/lib/helper/dates_and_times.dart
+++ app/lib/helper/dates_and_times.dart
@@ -84,9 +84,9 @@ extension Formatter on DateTime {
     }
   }
 	int getDaysInMonth() => switch (month) {
-			1 || 3 || 5 || 7 || 8 ||12 => 31,
-			4 || 6 || 9 || 10 || 11 => 30,
-			_ => isLeapYear() ? 28 : 29
+			1 || 3 || 5 || 7 || 8 || 10 || 12 => 31,
+			4 || 6 || 9 ||  11 => 30,
+			_ => isLeapYear() ? 29 : 28
 	};
 
 	bool isLeapYear() => !((year % 100 == 0) && (year % 400 != 0)) && (year % 4 == 0);
